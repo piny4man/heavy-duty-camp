c69e197d895674ecbd6c409aca4fd4132154f6ed
piny4man <development@pinya.dev>
2024-02-10 14:27:43 +0100

    feat: new landing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import '@fontsource/share-tech-mono';
 
 @Component({
   standalone: true,
